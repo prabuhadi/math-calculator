6c0ae6996ca961804ed2cb07fab4ef1d5cc41092
prabuhadi <prabuhadiyo@gmail.com>
2023-02-03 17:31:12 +0700

    Fixing minus bug

--- script.js
+++ script.js
@@ -37,7 +37,7 @@ function handleSymbol(symbol) {
       }
       break;
     case "+":
-    case "-":
+    case "−":
     case "×":
     case "÷":
       handleMath(symbol);
@@ -64,7 +64,7 @@ function handleMath(symbol) {
 function flushOperation(intBuffer) {
   if (previousOperator === "+") {
     runningTotal += intBuffer;
-  } else if (previousOperator === "-") {
+  } else if (previousOperator === "−") {
     runningTotal -= intBuffer;
   } else if (previousOperator === "×") {
     runningTotal *= intBuffer;
